26fe32fe900c862ad3162bad367b7a2e7c5348e9
pcJitsu <antonios.zikos1@gmail.com>
2023-08-07 21:45:33 -0500

    added Pseudo

---
+++ utils/generatelogo.js
@@ -0,0 +1,9 @@
+//Functons based off of primarily shape
+
+//Write File to Logo.SVG
+
+//Test to ensure not too many characters for color
+
+//same for the text.length
+
+//test for 3 valeus in shape
