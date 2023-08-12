7f9b160ec7085922abc45182cf20b0c0dd08bef7
pcJitsu <antonios.zikos1@gmail.com>
2023-08-12 17:57:43 -0500

    added comments

--- index.js
+++ index.js
@@ -1,32 +1,13 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
-const inquirer = require("inquirer");
-const generateLogo = require("./lib/generatelogo");
+const { userInput, getInput } = require("./lib/input");
+const { Triangle, Circle, Square } = require("./lib/shapes");
+const { userInfo } = require("os");
 
-//Generate Questions for the Prompts for Text/Characters/Shapes
-const questions = [];
+function generateLogo() {
+  const userInput = getInput();
 
-//Init Program
-function init() {
-  inquirer.prompt(questions).then((response) => {
-    console.log(response);
-    writeToFile("./utils/logo.svg", generateLogo(response));
-  });
+  //
 }
-//generateLogo();
 
-// ```md
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for text
-// THEN I can enter up to three characters
-// WHEN I am prompted for the text color
-// THEN I can enter a color keyword (OR a hexadecimal number)
-// WHEN I am prompted for a shape
-// THEN I am presented with a list of shapes to choose from: circle, triangle, and square
-// WHEN I am prompted for the shape's color
-// THEN I can enter a color keyword (OR a hexadecimal number)
-// WHEN I have entered input for all the prompts
-// THEN an SVG file is created named `logo.svg`
-// AND the output text "Generated logo.svg" is printed in the command line
-// WHEN I open the `logo.svg` file in a browser
-// THEN I am shown a 300x200 pixel image that matches the criteria I entered
+generateLogo();
